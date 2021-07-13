ddf3ba8305203bf399ef0b027ea3620d87b2fc61
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-13 17:26:32 +0530

    Create fighterbotrun.py

---
+++ fighterbotrun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/ComradeDear/FighterBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
