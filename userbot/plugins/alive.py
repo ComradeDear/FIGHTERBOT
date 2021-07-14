46f16c03ed65b4bfb42fec0346a6ff1881d33a46
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-14 11:14:19 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,32 +12,39 @@ from userbot.utils import lightning_cmd, edit_or_reply, sudo_cmd
 
 ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
 if ALIVE_PIC is None:
-    PM_iMG = "https://telegra.ph/file/d8d194c18562472a7c1f1.jpg"
+    PM_iMG = "https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg"
 else:
     PM_iMG = ALIVE_PIC
 
 
 HELL_PIC = os.environ.get("HELL_PIC", None)
 if HELL_PIC is None:
-    HELL_IMG = "https://telegra.ph/file/d8d194c18562472a7c1f1.jpg"
+    HELL_IMG = "https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg"
 else:
     HELL_IMG = HELL_PIC
 
 CAT_IMGE = os.environ.get("CAT_IMGE", None)
 if CAT_IMGE is None:
-    CAT_IMG = "https://telegra.ph/file/d8d194c18562472a7c1f1.jpg"
+    CAT_IMG = "https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg"
 else:
     CAT_IMG = CAT_IMGE
 
 version = "4.5"
 python_version = "3.8.5"
 catversion = "3.0"
-ALIVE_MSG = Config.ALIVE_MSG or "✮ HELLO MASTER I AM RUNNING SUCCESSFULLY ✮"
+ALIVE_MSG = Config.ALIVE_MSG or "✮ MY BOT IS RUNNING SUCCESFULLY ✮"
 EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
 hellversion = "7.0"
 # 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FIGHTERBOT"
 ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO", None)
+
+# Thanks to Sipak bro and Raganork..
+# animation Idea by @NOOB_GUY_OP (Sipakisking)
+# Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for DC(DARK COBRA)
+
 
 def get_readable_time(seconds: int) -> str:
     count = 0
@@ -79,7 +86,7 @@ async def reply_id(event):
 async def amireallyalive(salive):
     """ For .alive command, check if the bot is running.  """
     if ALIVE_PHOTTO:
-        pm_caption = "**FIGHTERBOT 𝙸𝚂 🅾︎🅽🅻🅸🅽🅴**\n"
+        pm_caption = "**FIGHTERBOT is online.\n"
         pm_caption += f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
         pm_caption += "𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽        : 1.17.5\n"
         pm_caption += "𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽          : 3.9.0\n"
@@ -100,7 +107,7 @@ async def amireallyalive(salive):
         )
         await sallive.delete()
         return
-    req = requests.get("https://telegra.ph/file/d8d194c18562472a7c1f1.jpg")
+    req = requests.get("https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg")
     req.raise_for_status()
     file = BytesIO(req.content)
     file.seek(0)
@@ -112,14 +119,14 @@ async def amireallyalive(salive):
         await borg.send_file(salive.chat_id, file=sticker)
         await borg.send_message(
             salive.chat_id,
-            "**FIGHTERBOT 𝙸𝚂 🅾︎🅽🅻🅸🅽🅴**\n"
+            "**вℓα¢к ℓιgнтηιηg 𝙸𝚂 🅾︎🅽🅻🅸🅽🅴**\n"
             f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
             "𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽        : 1.17.5\n"
             "𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽          : 3.9.0\n"
             "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻         : [ᴊᴏɪɴ](https://t.me/fighterbot_channel)\n"
             "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿           : [ᴊᴏɪɴ](https://t.me/fighterbot_support)\n"
             "𝘓𝘐𝘚𝘌𝘕𝘊𝘌                  : [AGPL-3.0  ʟɪᴄᴇɴꜱᴇ](https://jenaatul8.wixsite.com/hellboi-atul)\n"
-            "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔            : [ FIGHTERBOT ](https://t.me/dear_comradee)\n"
+            "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔            : [ вℓα¢к ℓιgнтηιηg ](https://t.me/dear_comradee)\n"
             "[ ┏┓━┏┓━━━━┏┓━┏┓━━━━━\n ┃┃━┃┃━━━━┃┃━┃┃━━━━━\n ┃┗━┛┃┏━━┓┃┃━┃┃━┏━━┓\n ┃┏━┓┃┃┏┓┃┃┃━┃┃━┃┏┓┃ \n ┃┃━┃┃┃┃━┫┃┗┓┃┗┓┃┗┛┃ \n ┗┛━┗┛┗━━┛┗━┛┗━┛┗━━┛](https://t.me/blacklightningot)",
             link_preview=False,
         )
@@ -140,7 +147,8 @@ else:
 kraken = bot.uid
 
 
-pm_caption = "__**🔥🔥FIGHTERBOT ɨs օռʟɨռɛ🔥🔥**__\n\n"
+pm_caption = "__**🔥🔥FI6
+🔥🔥**__\n\n"
 
 pm_caption += (
     f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『[{DEFAULTUSER}](tg://user?id={kraken})』**\n\n"
@@ -148,18 +156,18 @@ pm_caption += (
 
 pm_caption += "🛡️TELETHON🛡️ : `1.15.0` \n"
 
-pm_caption += f"FIGHTERBOT      : __**{hellversion}**__\n"
+pm_caption += f"😈ɮʟǟƈӄ ʟɨɢɦȶռɨռɢ😈       : __**{hellversion}**__\n"
 
 pm_caption += f"⚜️Sudo⚜️            : `{sudou}`\n"
 
-pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/fighterbot_support)\n"
+pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/lightning_support_group)\n"
 
-pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/dear_comradee)\n\n"
+pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/krish1303y)\n\n"
 
-pm_caption += "    [✨REPO✨](https://github.com/ComradeDear/FIGHTERBOT) 🔹 [📜License📜](https://github.com/ComradeDear/FIGHTERBOT/blob/master/LICENSE)"
+pm_caption += "    [✨REPO✨](https://github.com/KeinShin/Black-Lightningt) 🔹 [📜License📜](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)"
 
 
-@borg.on(fighterbot_cmd(outgoing=True, pattern="halive$"))
+@borg.on(lightning_cmd(outgoing=True, pattern="halive$"))
 @borg.on(sudo_cmd(pattern="halive$", allow_sudo=True))
 async def amireallyalive(halive):
     await halive.get_chat()
@@ -173,7 +181,7 @@ async def amireallyalive(halive):
 # Credits To catbot And Sandi
 
 
-@borg.on(fighterbot_cmd(outgoing=True, pattern="calive$"))
+@borg.on(lightning_cmd(outgoing=True, pattern="calive$"))
 @borg.on(sudo_cmd(pattern="calive$", allow_sudo=True))
 async def amireallyalive(calive):
     if calive.fwd_from:
@@ -185,7 +193,7 @@ async def amireallyalive(calive):
         pm_caption = f"**{ALIVE_MSG}**\n\n"
         pm_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
         pm_caption += f"**{EMOJI} Telethon version :** `{version}\n`"
-        pm_caption += f"**{EMOJI} FIGHTERBOT Version :** `{catversion}`\n"
+        pm_caption += f"**{EMOJI} Lightning Userbot Version :** `{catversion}`\n"
         pm_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
         pm_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
         pm_caption += f"**{EMOJI} Master:** {DEFAULTUSER}\n"
@@ -199,7 +207,7 @@ async def amireallyalive(calive):
             f"**{ALIVE_MSG}**\n\n"
             f"**{EMOJI} Database :** `{check_sgnirts}`\n"
             f"**{EMOJI} Telethon Version :** `{version}\n`"
-            f"**{EMOJI} FIGHTERBOT Version :** `{catversion}`\n"
+            f"**{EMOJI} Lightning Userbot Version :** `{catversion}`\n"
             f"**{EMOJI} Python Version :** `{python_version()}\n`"
             f"**{EMOJI} Uptime :** `{uptime}\n`"
             f"**{EMOJI} Master:** {DEFAULTUSER}\n",
@@ -234,7 +242,7 @@ from userbot.thunderconfig import Config
 CUSTOM_ALIVE = (
     Var.CUSTOM_ALIVE
     if Var.CUSTOM_ALIVE
-    else "Hey! I'm alive. I AM RUNNING SMOOTHLY!"
+    else "Hey! I'm alive. All systems online and functioning normally!"
 )
 
 
@@ -276,7 +284,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FIGHTERBOT"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "вℓα¢к ℓιgнтηιηg"
 
 
 CMD_HELP.update(
