99875244ec7c5abaa1b9a48856ae6b5b54e3bd57
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-14 21:43:00 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -1,118 +1,233 @@
 
-import os
-from time import sleep
-from urllib.parse import quote_plus
+"""
+✘ Commands Available -
+• `{i}carbon <text/reply to msg/reply to document>`
+    Carbonise the text with default settings.
+• `{i}rcarbon <text/reply to msg/reply to document>`
+    Carbonise the text, with random bg colours.
+"""
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import random
 
-from userbot import CHROME_DRIVER, GOOGLE_CHROME_BIN
-from userbot.utils import register
+import requests
+from carbonnow import Carbon
 
-CARBONLANG = "auto"
+from . import *
 
-LANG = "en"
+all_col = [
+    "Black",
+    "Navy",
+    "DarkBlue",
+    "MediumBlue",
+    "Blue",
+    "DarkGreen",
+    "Green",
+    "Teal",
+    "DarkCyan",
+    "DeepSkyBlue",
+    "DarkTurquoise",
+    "MediumSpringGreen",
+    "Lime",
+    "SpringGreen",
+    "Aqua",
+    "Cyan",
+    "MidnightBlue",
+    "DodgerBlue",
+    "LightSeaGreen",
+    "ForestGreen",
+    "SeaGreen",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "LimeGreen",
+    "MediumSeaGreen",
+    "Turquoise",
+    "RoyalBlue",
+    "SteelBlue",
+    "DarkSlateBlue",
+    "MediumTurquoise",
+    "Indigo  ",
+    "DarkOliveGreen",
+    "CadetBlue",
+    "CornflowerBlue",
+    "RebeccaPurple",
+    "MediumAquaMarine",
+    "DimGray",
+    "DimGrey",
+    "SlateBlue",
+    "OliveDrab",
+    "SlateGray",
+    "SlateGrey",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "MediumSlateBlue",
+    "LawnGreen",
+    "Chartreuse",
+    "Aquamarine",
+    "Maroon",
+    "Purple",
+    "Olive",
+    "Gray",
+    "Grey",
+    "SkyBlue",
+    "LightSkyBlue",
+    "BlueViolet",
+    "DarkRed",
+    "DarkMagenta",
+    "SaddleBrown",
+    "DarkSeaGreen",
+    "LightGreen",
+    "MediumPurple",
+    "DarkViolet",
+    "PaleGreen",
+    "DarkOrchid",
+    "YellowGreen",
+    "Sienna",
+    "Brown",
+    "DarkGray",
+    "DarkGrey",
+    "LightBlue",
+    "GreenYellow",
+    "PaleTurquoise",
+    "LightSteelBlue",
+    "PowderBlue",
+    "FireBrick",
+    "DarkGoldenRod",
+    "MediumOrchid",
+    "RosyBrown",
+    "DarkKhaki",
+    "Silver",
+    "MediumVioletRed",
+    "IndianRed ",
+    "Peru",
+    "Chocolate",
+    "Tan",
+    "LightGray",
+    "LightGrey",
+    "Thistle",
+    "Orchid",
+    "GoldenRod",
+    "PaleVioletRed",
+    "Crimson",
+    "Gainsboro",
+    "Plum",
+    "BurlyWood",
+    "LightCyan",
+    "Lavender",
+    "DarkSalmon",
+    "Violet",
+    "PaleGoldenRod",
+    "LightCoral",
+    "Khaki",
+    "AliceBlue",
+    "HoneyDew",
+    "Azure",
+    "SandyBrown",
+    "Wheat",
+    "Beige",
+    "WhiteSmoke",
+    "MintCream",
+    "GhostWhite",
+    "Salmon",
+    "AntiqueWhite",
+    "Linen",
+    "LightGoldenRodYellow",
+    "OldLace",
+    "Red",
+    "Fuchsia",
+    "Magenta",
+    "DeepPink",
+    "OrangeRed",
+    "Tomato",
+    "HotPink",
+    "Coral",
+    "DarkOrange",
+    "LightSalmon",
+    "Orange",
+    "LightPink",
+    "Pink",
+    "Gold",
+    "PeachPuff",
+    "NavajoWhite",
+    "Moccasin",
+    "Bisque",
+    "MistyRose",
+    "BlanchedAlmond",
+    "PapayaWhip",
+    "LavenderBlush",
+    "SeaShell",
+    "Cornsilk",
+    "LemonChiffon",
+    "FloralWhite",
+    "Snow",
+    "Yellow",
+    "LightYellow",
+    "Ivory",
+    "White",
+]
 
 
-@register(outgoing=True, pattern="^.carbon")
-async def carbon_api(e):
-
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-
-        """ A Wrapper for carbon.now.sh """
-
-        await e.edit("`Processing..`")
-
-        CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
-
-        global CARBONLANG
-
-        textx = await e.get_reply_message()
-
-        pcode = e.text
-
-        if pcode[8:]:
-
-            pcode = str(pcode[8:])
-
-        elif textx:
-
-            pcode = str(textx.message)  # Importing message to module
-
-        code = quote_plus(pcode)  # Converting to urlencoded
-
-        await e.edit("`Meking Carbon...\n25%`")
-
-        url = CARBON.format(code=code, lang=CARBONLANG)
-
-        chrome_options = Options()
-
-        chrome_options.add_argument("--headless")
-
-        chrome_options.binary_location = GOOGLE_CHROME_BIN
-
-        chrome_options.add_argument("--window-size=1920x1080")
-
-        chrome_options.add_argument("--disable-dev-shm-usage")
-
-        chrome_options.add_argument("--no-sandbox")
-
-        chrome_options.add_argument("--disable-gpu")
-
-        prefs = {"download.default_directory": "./"}
-
-        chrome_options.add_experimental_option("prefs", prefs)
-
-        driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
-
-        driver.get(url)
-
-        await e.edit("`Be Patient...\n50%`")
-
-        download_path = "./"
-
-        driver.command_executor._commands["send_command"] = (
-            "POST",
-            "/session/$sessionId/chromium/send_command",
+@fighterbot_cmd(
+    pattern="carbon",
+)
+async def crbn(event):
+    xxxx = await eor(event, get_string("com_1"))
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            b = await ultroid_bot.download_media(temp)
+            a = open(b)
+            code = a.read()
+            a.close()
+            os.remove(b)
+        else:
+            code = temp.message
+    else:
+        code = event.text.split(" ", maxsplit=1)[1]
+    webs = requests.get("https://carbonara.vercel.app/api/cook")
+    if webs.status_code == 502:
+        return await eor(
+            event, "`Temporary Server Error has Occured !\nPlease Try Again Later`"
         )
+    carbon = Carbon(base_url="https://carbonara.vercel.app/api/cook", code=code)
+    xx = await carbon.memorize("ultroid_carbon")
+    await xxxx.delete()
+    await ultroid_bot.send_file(
+        event.chat_id,
+        xx,
+        caption=f"Carbonised by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+    )
 
-        params = {
-            "cmd": "Page.setDownloadBehavior",
-            "params": {"behavior": "allow", "downloadPath": download_path},
-        }
 
-        driver.execute("send_command", params)
-
-        driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-
-        # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-
-        # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-
-        await e.edit("`Processing..\n75%`")
-
-        # Waiting for downloading
-
-        sleep(2.5)
-
-        await e.edit("`Done Dana Done...\n100%`")
-
-        file = "./carbon.png"
-
-        await e.edit("`Uploading..`")
-
-        await e.client.send_file(
-            e.chat_id,
-            file,
-            caption="<< Here's your carbon, \n Carbonised by [Black Lightning](https://www.github.com/hellboi-atul/hellboi-atul)>> ",
-            force_document=True,
-            reply_to=e.message.reply_to_msg_id,
+@fighterbot_cmd(
+    pattern="rcarbon",
+)
+async def crbn(event):
+    xxxx = await eor(event, "Processing")
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            b = await ultroid_bot.download_media(temp)
+            a = open(b)
+            code = a.read()
+            a.close()
+            os.remove(b)
+        else:
+            code = temp.message
+    else:
+        code = event.text.split(" ", maxsplit=1)[1]
+    col = random.choice(all_col)
+    webs = requests.get("https://carbonara.vercel.app/api/cook")
+    if webs.status_code == 502:
+        return await eor(
+            event, "`Temporary Server Error has Occured !\nPlease Try Again Later`"
         )
-
-        os.remove("./FRIDAY.png")
-
-        driver.quit()
-
-        # Removing carbon.png after uploading
-
-        await e.delete()  # Deleting msg
+    carbon = Carbon(
+        base_url="https://carbonara.vercel.app/api/cook", code=code, background=col
+    )
+    xx = await carbon.memorize("ultroid_carbon")
+    await xxxx.delete()
+    await ultroid_bot.send_file(
+        event.chat_id,
+        xx,
+        caption=f"Carbonised by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+    )
