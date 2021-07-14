d409e3d52a3c0a7dd34ed3c8e864e137fde7fcd5
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-15 00:17:45 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,7 +9,6 @@ from telethon import TelegramClient
 
 from userbot import CMD_HNDLR, bot
 from userbot.config import Var
-from userbot.thunderconfig import Config
 
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
