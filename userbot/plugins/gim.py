0a7b2d61a8c24479451a245aad9c3d455fa2b1b8
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-07-18 01:07:18 +0530

    Create gim.py

---
+++ userbot/plugins/gim.py
@@ -0,0 +1,14 @@
+#Plugin by @Rishisuperyo
+#Animation by kiddo
+#kang =gey ,keep credits = cool coder 😶
+#usage .gim
+from telethon import events
+import asyncio
+from userbot.utils import fighter_cmd
+from userbot import CMD_HELP
+@borg.on(fighter_cmd(pattern=r"gim", outgoing=True))
+async def hapy(event):
+
+     a="🎱➖✊➖➖✊➖🎱\n🌟        \         /          🌟\n⭐          \😁/            ⭐\n✨           🎽             ✨\n              /    \ \n            👟    👟"
+     await event.edit(a)
+	
