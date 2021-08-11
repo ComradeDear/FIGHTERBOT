d02fbdb4160042079dc0b2bfe0c2ecd57cfcafd7
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-08-11 16:24:45 +0530

    Update fighterbotrun.py

--- fighterbotrun.py
+++ fighterbotrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/ComradeDear/FighterBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/ComradeDear/FighterBot/root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
