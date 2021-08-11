be424868861bd9ac82c956fed22d5da2d2223ba9
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-08-11 16:12:05 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -2,12 +2,12 @@
 """
 from userbot import ALIVE_NAME
 from userbot.Config import Var
-from userbot.utils import admin_cmd
+from userbot.utils import fighter_cmd
 import os
 ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO", None)
 if not ALIVE_PHOTTO:
-    ALIVE_PHOTTO = "https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg"
-
+    ALIVE_PHOTTO = "
+https://telegra.ph/file/3bc2b89fd6fe407ebcaeb.jpg"
 
 DEFAULTUSER = (
     str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
@@ -20,13 +20,12 @@ if not ALIVE_MESSAGE:
     ALIVE_MESSAGE += f"`Telethon: TELETHON-15.0.0 \n\n`"
     ALIVE_MESSAGE += f"`Python: PYTHON-3.8.5 \n\n`"
     ALIVE_MESSAGE += "`I'll Be With You Master Till My Last Breathe!!☠ \n\n`"
-    ALIVE_MESSAGE += f"`Support Channel` : @fighterbot_support \n\n"
     ALIVE_MESSAGE += f"`MY BOSS🤗`: {DEFAULTUSER} \n\n "
 else:
     ALIVE_MESSAGE = ALIVE_MESSAGE
 
 # @command(outgoing=True, pattern="^.awake$")
-@borg.on(admin_cmd(pattern=r"awake"))
+@borg.on(fighter_cmd(pattern=r"awake"))
 async def amireallyalive(awake):
     """ For .awake command, check if the bot is running.  """
     await awake.delete()
